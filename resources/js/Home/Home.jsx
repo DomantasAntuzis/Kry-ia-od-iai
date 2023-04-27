39fa573a553bed2643a70a81ac8fde29ba299ce5
Domantuzis <antuzisdomantas@gmail.com>
2023-04-27 11:45:38 +0300

    crossword and other changes

--- resources/js/Home/Home.jsx
+++ resources/js/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Registration from "../Forms/registration";
 import Login from "../Forms/login";
 import Crossword from "../Forms/crossword";
@@ -7,7 +7,7 @@ export default function Home() {
     const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
     const [showRegister, setShowRegister] = useState(false);
     const [showLogin, setShowLogin] = useState(false);
-    const [apiToken, setApiToken] = useState('');
+    const [apiToken, setApiToken] = useState("");
     const [isLoggedIn, setIsLoggedIn] = useState(
         localStorage.getItem("isLoggedIn") === "true"
     );
@@ -41,65 +41,67 @@ export default function Home() {
     //logout logic
 
     const handleLogout = () => {
-        // console.log(csrfToken)
-        fetch('api/logout', {
-            method: 'POST',
+        fetch("api/logout", {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
+                "Content-Type": "application/json",
+                Accept: "application/json",
                 "X-CSRF-TOKEN": csrfToken,
-                'Authorization': `Bearer ${apiToken}`
+                Authorization: `Bearer ${apiToken}`,
             },
             body: JSON.stringify({}),
         })
             .then((response) => {
                 if (response.ok) {
-                    localStorage.removeItem('IsLoggedIn');
+                    localStorage.removeItem("isLoggedIn");
                     setIsLoggedIn(false);
-                    console.log('User logged out successfully.');
+                    console.log("User logged out successfully.");
                 } else {
-                    throw new Error('Logout failed.');
+                    throw new Error("Logout failed.");
                 }
             })
 
-            // //this should be before fetch
-            // .then((data) => {
-            //     // Access the data in the response
-            //     // console.log(data.message);
-            //     const token = data.sukurtas_token;
-            //     console.log("Naujas_Token", token);
-            // })
-
             .catch((error) => {
-                console.error('Logout failed.', error);
+                console.error("Logout failed.", error);
             });
     };
 
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
-
-
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">
                         Navbar
                     </a>
                     <div className="d-flex">
-                        <button type="button" className="btn btn-outline-success">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                                 className="bi bi-plus-lg" viewBox="0 0 18 18">
-                                <path fillRule="evenodd"
-                                      d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                        {isLoggedIn ? (
+                        <div>
+                        <button
+                            type="button"
+                            className="btn btn-outline-success"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                fill="currentColor"
+                                className="bi bi-plus-lg"
+                                viewBox="0 0 18 18"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+                                />
                             </svg>
                             Crossword
                         </button>
-                        {isLoggedIn ? (
                             <button
                                 className="btn btn-outline-success"
                                 onClick={handleLogout}
                             >
                                 Log Out
                             </button>
+                            </div>
                         ) : (
                             <div>
                                 <button
@@ -121,9 +123,10 @@ export default function Home() {
             </nav>
 
             {showRegister && !isLoggedIn && <Registration></Registration>}
-            {showLogin && !isLoggedIn && <Login setApiToken={setApiToken}></Login>}
-            {/*<Crossword setApiToken={setApiToken} ></Crossword>*/}
-
+            {showLogin && !isLoggedIn && (
+                <Login setApiToken={setApiToken}></Login>
+            )}
+            {isLoggedIn && <Crossword setApiToken={setApiToken}></Crossword>}
         </>
     );
 }
