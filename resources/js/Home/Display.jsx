3685571b4c69b7ecbb23215db3cef86850630cdf
Domantuzis <antuzisdomantas@gmail.com>
2023-04-28 13:33:53 +0300

    crossword v1 nebaigtas

--- resources/js/Home/Display.jsx
+++ resources/js/Home/Display.jsx
@@ -1,90 +1,198 @@
 import React, { useState, useEffect } from "react";
 import "../../scss/styles.scss";
 
-const words = [
-  "zodis",
-  "grazu",
-  "veikia",
-  "puikiai",
-  "namas",
-  "super",
-  "testas"
-];
-
 const GRID_SIZE = { rows: 10, cols: 15 };
 
 export default function Home() {
-  const [grid, setGrid] = useState([]);
+  const [grid, setGrid] = useState(
+    Array.from({ length: GRID_SIZE.rows }).map(() =>
+      Array.from({ length: GRID_SIZE.cols }).fill({ letter: null, selected: false })
+    )
+  );
+  const [words, setWords] = useState([]);
+  const [selectedWord, setSelectedWord] = useState(null);
 
   useEffect(() => {
-    const newGrid = Array.from({ length: GRID_SIZE.rows }).map(() =>
-      Array.from({ length: GRID_SIZE.cols }).fill(null)
+    setGrid(
+      Array.from({ length: GRID_SIZE.rows }).map(() =>
+        Array.from({ length: GRID_SIZE.cols }).fill({ letter: null, selected: false })
+      )
     );
-
-    // word directions
-    words.forEach((word) => {
-      const directions = [
-        [-1, 0], // up
-        [0, -1], // left
-        [1, 0], // down
-        [0, 1], // right
-      ];
-
-      const row = Math.floor(Math.random() * GRID_SIZE.rows);
-      const col = Math.floor(Math.random() * GRID_SIZE.cols);
-      const [dRow, dCol] = directions[Math.floor(Math.random() * 4)];
-
-      //word placement
-      let fits = true;
-      for (let i = 0; i < word.length; i++) {
-        const r = row + i * dRow;
-        const c = col + i * dCol;
-        if (r < 0 || r >= GRID_SIZE.rows || c < 0 || c >= GRID_SIZE.cols) {
-          fits = false;
-          break;
-        }
-        if (newGrid[r][c] !== null && newGrid[r][c] !== word[i]) {
-          fits = false;
-          break;
-        }
-      }
-
-      if (fits) {
-        for (let i = 0; i < word.length; i++) {
-          const r = row + i * dRow;
-          const c = col + i * dCol;
-          newGrid[r][c] = word[i];
-        }
-      }
-    });
-
-    // Fill the remaining spaces with random letters
-    for (let r = 0; r < GRID_SIZE.rows; r++) {
-      for (let c = 0; c < GRID_SIZE.cols; c++) {
-        if (newGrid[r][c] === null) {
-          newGrid[r][c] = String.fromCharCode(
-            Math.floor(Math.random() * 26) + 65
-          );
-        }
-      }
-    }
-
-    setGrid(newGrid);
   }, []);
 
+  const handleCellClick = (row, col) => {
+    setGrid((prevGrid) => {
+      const newGrid = [...prevGrid];
+      newGrid[row][col].selected = !newGrid[row][col].selected;
+
+      const word = getSelectedWord(newGrid, row, col);
+      setSelectedWord(word);
+
+      return newGrid;
+    });
+  };
+
+  const handleAddWord = (event) => {
+    event.preventDefault();
+    const input = event.target.elements.word;
+    const newWord = input.value;
+    setWords([
+      ...words,
+      {
+        word: newWord,
+        direction: "across",
+        startRow: 0,
+        startCol: 0,
+      },
+    ]);
+    input.value = "";
+  };
+
+  const getSelectedWord = (grid, row, col) => {
+    // Check if there is a selected word across
+    let startCol = col;
+    while (startCol > 0 && grid[row][startCol - 1].selected) {
+      startCol--;
+    }
+    let endCol = col;
+    while (endCol < GRID_SIZE.cols - 1 && grid[row][endCol + 1].selected) {
+      endCol++;
+    }
+    if (endCol > startCol) {
+      return {
+        word: grid[row].slice(startCol, endCol + 1).map((cell) => cell.letter).join(""),
+        direction: "across",
+        startRow: row,
+        startCol,
+      };
+    }
+
+    // Check if there is a selected word down
+    let startRow = row;
+    while (startRow > 0 && grid[startRow - 1][col].selected) {
+      startRow--;
+    }
+    let endRow = row;
+    while (endRow < GRID_SIZE.rows - 1 && grid[endRow + 1][col].selected) {
+      endRow++;
+    }
+    if (endRow > startRow) {
+      return {
+        word: grid.slice(startRow, endRow + 1).map((row) => row[col].letter).join(""),
+        direction: "down",
+        startRow,
+        startCol: col,
+      };
+    }
+    
+    return null;
+  };
+
+  const handleAddSelectedWord = (selectedWord) => {
+    const startRow = prompt("Enter starting row:");
+    const startCol = prompt("Enter starting column:");
+  
+    // Convert startRow and startCol to numbers
+    const startRowIndex = parseInt(startRow);
+    const startColIndex = parseInt(startCol);
+  
+    // Add the selected word to the grid
+    setGrid((prevGrid) => {
+      const newGrid = [...prevGrid];
+  
+      // Add the word horizontally
+      if (selectedWord.direction === "across") {
+        for (let col = 0; col < selectedWord.word.length; col++) {
+          const letter = selectedWord.word.charAt(col);
+          newGrid[startRowIndex][startColIndex + col] = { letter, selected: false };
+        }
+      }
+  
+      // Add the word vertically
+      else {
+        for (let row = 0; row < selectedWord.word.length; row++) {
+          const letter = selectedWord.word.charAt(row);
+          newGrid[startRowIndex + row][startColIndex] = { letter, selected: false };
+        }
+      }
+  
+      return newGrid;
+    });
+  };
+  
+  
+
+  const handleWordSelection = (word) => {
+  setSelectedWord(word);
+   handleAddSelectedWord(word);
+  console.log(word); 
+  };
+  
+  const handleWordChange = (event, i, key) => {
+  const newWords = [...words];
+  newWords[i][key] = event.target.value;
+  setWords(newWords);
+  };
+  
   return (
-    <div className="table-container">
-      <table>
-        <tbody>
-          {grid.map((row, i) => (
-            <tr key={i}>
-              {row.map((cell, j) => (
-                <td key={`${i}-${j}`}>{cell}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+  <div className="crossword">
+  <div className="table-container">
+  <table>
+  <tbody>
+  {grid.map((row, i) => (
+  <tr key={i}>
+  {row.map(({ letter, selected }, j) => (
+  <td
+  key={`${i}-${j}`}
+  className={selected ? "selected" : ""}
+  data-row={i}
+  data-col={j}
+  onClick={() => handleCellClick(i, j)}
+  >
+  {letter}
+  </td>
+  ))}
+  </tr>
+  ))}
+  </tbody>
+  </table>
+  </div>
+  <form onSubmit={handleAddWord}>
+  <input type="text" name="word" placeholder="Enter a word" />
+  <button type="submit">Add</button>
+  </form>
+  <div>
+  <h2>Words</h2>
+  <ul>
+  {words.map((word, i) => (
+  <li key={i}>
+  <input
+  type="text"
+  value={word.word}
+  onChange={(event) => handleWordChange(event, i, "word")}
+  />
+  <select
+  value={word.direction}
+  onChange={(event) => handleWordChange(event, i, "direction")}
+  >
+  <option value="across">Across</option>
+  <option value="down">Down</option>
+  </select>
+  <button onClick={() => handleWordSelection(word)}>Select</button>
+  </li>
+  ))}
+  </ul>
+  </div>
+  {selectedWord && (
+  <div>
+  <h2>Selected Word</h2>
+  <p>Word: {selectedWord.word}</p>
+  <p>Direction: {selectedWord.direction}</p>
+  <p>Start Row: {selectedWord.startRow}</p>
+  <p>Start Col: {selectedWord.startCol}</p>
+  </div>
+  )}
+  </div>
   );
-}
+};
+
